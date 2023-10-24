c5596d713e41784c9c950d387788410988c6a8db
erikanormande <2022436@student.cct.ie>
2023-10-24 18:28:59 +0100

    Erika

--- Mini3/src/mini3/TeamCreator.java
+++ Mini3/src/mini3/TeamCreator.java
@@ -1,3 +1,5 @@
+//link github
+//https://github.com/erikanormande/HomeWork/tree/main/Mini3
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
